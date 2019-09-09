0f04847464a4b50333292cd0527fc022275ee162
travhow99 <trav.horn@gmail.com>
2019-09-09 10:52:13 +0200

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
